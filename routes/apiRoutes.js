fbb12cd1f5ecc0bc8d02d2d5e3c3b669eea1aaa9
jancypp <120283327+jancypp@users.noreply.github.com>
2023-03-16 15:51:37 -0600

    updated API routes to remove list for delete

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,43 +2,39 @@ const app = require("express").Router();
 const fs = require("fs");
 let db = require("../db/db.json");
 
-app.get('/api/notes',(req,res) => {
-   db = JSON.parse(fs.readFileSync("./db/db.json")) || []
-   res.json(db);
+app.get('/api/notes', (req, res) => {
+    db = JSON.parse(fs.readFileSync("./db/db.json")) || []
+    res.json(db);
 });
 
-app.post('/api/notes',(req,res) => {
+app.post('/api/notes', (req, res) => {
     let newNote = {
-        id: Math.floor(Math.random()*1000), 
+        id: Math.floor(Math.random() * 1000),
         title: req.body.title,
         text: req.body.text,
     }
     db.push(newNote)
-    fs.writeFileSync("./db/db.json",JSON.stringify(db),function(err){
-        if(err) throw err
+    fs.writeFileSync("./db/db.json", JSON.stringify(db), function (err) {
+        if (err) throw err
     })
     res.json(db);
- });
+});
 
- app.delete('/api/notes/:id',(req,res) => {
-    let notDeletedNotes =[]
-     db.forEach(note => {
-        if(note.id != req.params.id ) {
+app.delete('/api/notes/:id', (req, res) => {
+    let notDeletedNotes = []
+    db.forEach(note => {
+        if (note.id != req.params.id) {
             notDeletedNotes.push(note)
-        } 
+        }
     })
-
-    let list = db.filter(note => note.id != req.prams.id)
-
-
-    console.log(notDeletedNotes, list)
-    db = notDeletedNotes 
-    fs.writeFileSync("./db/db.json",JSON.stringify(db),function(err){
-        if(err) throw err
+    console.log(notDeletedNotes,)
+    db = notDeletedNotes
+    fs.writeFileSync("./db/db.json", JSON.stringify(db), function (err) {
+        if (err) throw err
     })
     res.json(db);
- });
+});
 
 
 
- module.exports = app
+module.exports = app
