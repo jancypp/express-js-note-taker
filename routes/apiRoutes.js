aef6344529043c10de42c230c17da3622c77365e
jancypp <120283327+jancypp@users.noreply.github.com>
2023-03-16 15:46:47 -0600

    updated routes, serverjs file and linked files

---
+++ routes/apiRoutes.js
@@ -0,0 +1,44 @@
+const app = require("express").Router();
+const fs = require("fs");
+let db = require("../db/db.json");
+
+app.get('/api/notes',(req,res) => {
+   db = JSON.parse(fs.readFileSync("./db/db.json")) || []
+   res.json(db);
+});
+
+app.post('/api/notes',(req,res) => {
+    let newNote = {
+        id: Math.floor(Math.random()*1000), 
+        title: req.body.title,
+        text: req.body.text,
+    }
+    db.push(newNote)
+    fs.writeFileSync("./db/db.json",JSON.stringify(db),function(err){
+        if(err) throw err
+    })
+    res.json(db);
+ });
+
+ app.delete('/api/notes/:id',(req,res) => {
+    let notDeletedNotes =[]
+     db.forEach(note => {
+        if(note.id != req.params.id ) {
+            notDeletedNotes.push(note)
+        } 
+    })
+
+    let list = db.filter(note => note.id != req.prams.id)
+
+
+    console.log(notDeletedNotes, list)
+    db = notDeletedNotes 
+    fs.writeFileSync("./db/db.json",JSON.stringify(db),function(err){
+        if(err) throw err
+    })
+    res.json(db);
+ });
+
+
+
+ module.exports = app
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3001;
+const htmlRoutes = require("./routes/htmlRoutes");
+const apiRoutes = require("./routes/apiRoutes");
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use(htmlRoutes);
+app.use(apiRoutes);
+
+app.listen(PORT, ()=>{
+console.log(`App listening at http://localhost:${PORT}`)
+}); 
